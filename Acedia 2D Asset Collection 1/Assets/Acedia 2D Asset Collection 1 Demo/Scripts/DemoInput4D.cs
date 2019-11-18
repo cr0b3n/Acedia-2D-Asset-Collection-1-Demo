3a7f677e2500cf41918281d8dfd395a460855d4b
croben <omega.croben@gmail.com>
2019-11-18 20:20:59 +0800

    Partial Demo

---
+++ Acedia 2D Asset Collection 1/Assets/Acedia 2D Asset Collection 1 Demo/Scripts/DemoInput4D.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[DisallowMultipleComponent]
+public class DemoInput4D : MonoBehaviour {
+
+    [HideInInspector] public float horizontal;      //Float that stores horizontal input
+    [HideInInspector] public float vertical;        //Float that stores vertical input
+    [HideInInspector] public bool attackPressed;
+
+    private bool readyToClear;                      //Bool used to keep input in sync
+    [HideInInspector] public bool isActive;
+    private void Update() {
+        //Clear out existing input values
+        ClearInput();
+
+        //Process keyboard, mouse, gamepad (etc) inputs
+        ProcessInputs();
+
+        //Clamp the horizontal & vertical input to be between -1 and 1
+        horizontal = Mathf.Clamp(horizontal, -1f, 1f);
+        vertical = Mathf.Clamp(vertical, -1f, 1f);
+    }
+
+    private void FixedUpdate() {
+        //In FixedUpdate() we set a flag that lets inputs to be cleared out during the 
+        //next Update(). This ensures that all code gets to use the current inputs
+        readyToClear = true;
+    }
+
+    private void ClearInput() {
+        //If we're not ready to clear input, exit
+        if (!readyToClear)
+            return;
+
+        //Reset all inputs
+        horizontal = 0f;
+        vertical = 0f;
+        attackPressed = false;
+
+        readyToClear = false;
+    }
+
+    private void ProcessInputs() {
+        //Accumulate horizontal & vertical axis input
+        horizontal += Input.GetAxis("Horizontal");
+        vertical += Input.GetAxis("Vertical");
+
+        //Avoid attacking while pressing a button
+        if (!EventSystem.current.IsPointerOverGameObject()) {
+            attackPressed = attackPressed || Input.GetKeyDown(KeyCode.Space);
+        }
+    }
+}
---
+++ Acedia 2D Asset Collection 1/Assets/Acedia 2D Asset Collection 1 Demo/Scripts/Scriptable Objects/Movement/DemoHorizontalMoveSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Demo Horizontal Movement", menuName = "Acedia Demo/ Movement/ Horizontal Movement")]
+public class DemoHorizontalMoveSO : DemoMovementSO {
+
+    public float minXPosition = -7;
+    public float maxXPosition = 7;
+
+    public override void UpdateMovementData(DemoAutoMovementController controller) {
+        controller.UpdateBehaviour(new Vector2(Random.Range(minXPosition, maxXPosition), controller.transform.position.y));
+    }
+}
